82da737176cf8d1e969147cf42bdae7bb357839d
zainabnaima <zainabnaima7@gmail.com>
2023-10-11 10:56:20 +0400

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,11 @@
+name = "\tJohn Doe\n"
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+
+print("Using rstrip():")
+print(name.rstrip())
+
+print("Using strip():")
+print(name.strip())
